15ce8ed18a99fd7980e0117fdd408a4a44b32eef
yehudakahana <yk3222145@gmail.com>
2024-09-26 09:55:47 +0300

    started

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import authRouter from './routes/auth.js';
+import cors from 'cors';
+// import dotenv from 'dotenv';
+// dotenv.config();
+const PORT = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", authRouter);
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
